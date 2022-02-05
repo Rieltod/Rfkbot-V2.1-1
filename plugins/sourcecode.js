ce9ba5687665eaed1286e9ddbc0c2ddfb9da8058
NoDigi <98347766+NoDigi@users.noreply.github.com>
2022-02-05 18:20:23 +0800

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -2,10 +2,10 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
 *────────「 SCRIPT 」 ────────*
 Hai 👋
-saya AlyaaXzy_1
-Bot Ini Menggunakan Sc dari github
--
-`.trim(), '© AlyaaXzy', 'Thanks', '.tqto')
+saya LanXzy🥀
+Bot ini menggunakan sc autoresponder🗿
+-asli bg ga tipu🗿wkwkwk
+`.trim(), '© LynXzy', 'Thanks', '.tqto')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
