a04a416e5ef892beb094bade9eea701e3851fe4a
Oғғɪᴄɪᴀʟ Bᴏᴛ Lʏɴ乂ᴢʏ   ? <98347766+NoDigi@users.noreply.github.com>
2022-02-20 16:05:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -68,8 +68,8 @@ namalu = 'LynXzy🥀'
 
 
 // 
-wait = '_*Tunggu ya kontol...*_'
-global.wait = '_*Tunggu ya kontol...*_'
+wait = '_*Tunggu ya ...*_'
+global.wait = '_*Tunggu ya ...*_'
 global.rpg = 'Fitur Rpg Dimatikan\nKetik *!enable* *rpg* untuk menggunakan fitur ini!\nKalo Mau main Disini aja\nhttps://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9'
 global.nsfw = 'Fitur NSFW Dimatikan\nKetik *!enable* *nsfw* untuk menggunakan fitur ini!\n“Katakanlah kepada orang laki-laki yang beriman: Hendaklah mereka menahan pandanganya, dan memelihara kemaluannya; … Katakanlah kepada wanita yang beriman: Hendaklah mereka menahan pandangannya, dan kemaluannya, dan janganlah mereka Menampakkan perhiasannya, kecuali yang (biasa) nampak dari padany,” \n(TQS. Al-Nur [24]: 30-31).'
 global.eror = '_*Server Error*_'
