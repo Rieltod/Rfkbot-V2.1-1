b12c0473e3763a903fc36d0aebbb7cd01ffc09dc
Oғғɪᴄɪᴀʟ Bᴏᴛ Lʏɴ乂ᴢʏ   ? <98347766+NoDigi@users.noreply.github.com>
2022-02-20 15:58:30 +0800

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,10 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
 *────────「 SCRIPT 」 ────────*
-Hai 👋
-saya LanXzy🥀
-Eaa nyari sc ya banh?🗿
--
+Eaa nyari sc ya ?🗿
+Desah Dulu
 `.trim(), '© LynXzy', 'Thanks', '.tqto')
 handler.help = ['sourcecode']
 handler.tags = ['info']
