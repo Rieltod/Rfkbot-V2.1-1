2e27892d1eeb31e30a018b1d833f6963513f4399
Oғғɪᴄɪᴀʟ Bᴏᴛ Lʏɴ乂ᴢʏ   ? <98347766+NoDigi@users.noreply.github.com>
2022-03-02 22:02:23 +0800

    Update viadana.js

--- plugins/viadana.js
+++ plugins/viadana.js
@@ -1,29 +1,12 @@
 let handler = async (m, { conn }) => {
 	conn.reply(m.chat, `╠═〘 PEMBAYARAN 〙 ═
-╠═ Untuk Pembayaran Via DANA
-╠➥ Ke nomer ini
-║➥ 085730903853
-║
-╠═〘 Fitur 〙 ═
-║
-╠➥ *FITUR DOWNLOADER 500++
-║     YouTube, Twitter, 
-║     Tiktok, Instagram, dll.
-║    *FITUR STIKER*
-╠➥ *FITUR ADMIN*
-║    *KICK ORANG*
-║    *JADIIN ADMIN*
-╠➥ *FITUR ISLAM*
-║   *QURAN*
-║   *CEK JADWAL SHALAT*
-╠➥ *INTERNET*
-║   *BRAINLY*
-║   *GOOGLE*
-╠➥ *DAN 200 LEBIH*
-║   *FITUR LAINNYA*
+╠═ Untuk Pembayaran Via Digi
+╠➥ 60149431385
+║➥ Chat owner terlebih dahulu
+║- wa.me/60189830350
 ║ 
-╠═ ©2021 Rpg wabot-aq
-╠═ Scrip original by Nurutomo
+╠═ © LanXzy & LynXzy
+╠═ Thanks
 ╠═〘 ${namabot} 〙 ═`.trim(), m)
 }
 
