ffabe29bcc24298187be27563dd9c4f71e1abc78
Oғғɪᴄɪᴀʟ Bᴏᴛ Lʏɴ乂ᴢʏ   ? <98347766+NoDigi@users.noreply.github.com>
2022-03-02 21:58:43 +0800

    Update viagopay.js

--- plugins/viagopay.js
+++ plugins/viagopay.js
@@ -1,32 +1,15 @@
 let handler = async (m, { conn }) => {
 	conn.reply(m.chat, `╠═〘 PEMBAYARAN 〙 ═
-╠═ Untuk Pembayaran Via Gopay
-╠➥ Ke nomer ini
-║➥ 085730903853
+╠═ Untuk Pembayaran Via Umobile
+║➥ 60189830350
+║- chat owner terlebih dahulu
+║- wa.me/60189830350
 ║
-╠═〘 Fitur 〙 ═
-║
-╠➥ *FITUR DOWNLOADER 500++
-║     YouTube, Twitter, 
-║     Tiktok, Instagram, dll.
-║    *FITUR STIKER*
-╠➥ *FITUR ADMIN*
-║    *KICK ORANG*
-║    *JADIIN ADMIN*
-╠➥ *FITUR ISLAM*
-║   *QURAN*
-║   *CEK JADWAL SHALAT*
-╠➥ *INTERNET*
-║   *BRAINLY*
-║   *GOOGLE*
-╠➥ *DAN 200 LEBIH*
-║   *FITUR LAINNYA*
-║ 
-╠═ ©2021 Rpg wabot-aq
-╠═ Scrip original by Nurutomo
+╠═ © LanXzy & LynXzy
+╠═ Thanks✨
 ╠═〘 ${namabot} 〙 ═`.trim(), m)
 }
 
-handler.command = /^viagopay$/i
+handler.command = /^viaumobile$/i
 
 module.exports = handler
